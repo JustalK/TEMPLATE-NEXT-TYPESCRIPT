91cf05b8334dde78a9de3dbdcb20e2fadc481562
kevinjustal <kevin.justal@smood.ch>
2021-10-29 17:16:45 +0200

    [FEATURE] Adding better-doc

---
+++ src/types/TestProps.ts
@@ -0,0 +1,15 @@
+/**
+ * @typedef {Object} TestProps
+ */
+type TestProps = {
+  /**
+   * Id of Test
+   */
+  _id: string;
+  /**
+   * Test of test
+   */
+  test: string;
+};
+
+export default TestProps;
